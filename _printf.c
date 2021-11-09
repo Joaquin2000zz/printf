d37a307893ff2efc7ce34ba92ee3c73c7a975eae
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-11-08 20:57:55 -0800

    solving errors

--- _printf.c
+++ _printf.c
@@ -17,25 +17,22 @@ int _printf(const char *format, ...)
 	va_start(arg, format);
 	for (end_pos = 0; format[end_pos]; end_pos++)
 	{
-		if (format[end_pos + 1] == '\0' && format[end_pos] == '%')
-			return (-1);
 		if (format[end_pos] == '%')
 		{
+			if (!format[end_pos + 1])
+				return (-1);
 			if (format[end_pos + 1] == '%')
 			{
-				length++;
-				_putchar(format[end_pos]);
-				end_pos;
+				length++, _putchar('%'), end_pos++;
 				continue;
 			}
 			for (op_pos = 0; fstruct(op_pos).op; op_pos++)
 				if (fstruct(op_pos).op == format[1 + end_pos])
 				{
-					end_pos++;
 					length += fstruct(op_pos).f(arg);
 					break;
 				}
-			if (fstruct(op_pos).op == '\0')
+			if (!fstruct(op_pos).op)
 			{
 				_putchar(format[end_pos]);
 				length++;

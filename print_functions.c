3fde4f8dc9bbffaa91c730892565c056673f661d
Sahin <sahin.meric86@gmail.com>
2021-11-09 10:15:00 -0800

    print_functions fix

--- print_functions.c
+++ print_functions.c
@@ -34,3 +34,40 @@ int op_str(va_list arg)
 			_putchar(s[i]);
 	return (i);
 }
+
+/**
+ * aux_dec - return a int converted in string
+ * @n: int to convert
+ *
+ */
+
+int aux_dec(int n)
+{
+	int largo = 0;
+	unsigned int aux = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		aux = -n;
+		largo++;
+	}
+	else
+		aux = n;
+	if (n / 10)
+		largo += aux_dec(aux / 10);
+	_putchar((aux % 10) + 48);
+	largo++;
+	return (largo);
+}
+/**
+  * op_int - return a interger to convert with aux_dec to char
+  * @arg: arg to print
+  * Return: a interger
+  */
+int op_int(va_list arg)
+{
+	int i = va_arg(arg, int);
+
+	return(aux_dec(i));
+}

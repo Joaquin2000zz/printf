22ff6091c03135f20b02c895a41d1bd3907fc426
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-11-09 17:40:08 -0800

    reupdating files

--- main.h
+++ main.h
@@ -1,16 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
 int op_ch(va_list arg);
 int op_str(va_list arg);
 int op_bin(va_list arg);
-int op_rev(va_list arg);
 int _printf(const char *format, ...);
 int _putchar(char c);
+char *freear(char *final);
+
 
 /**
- * struct objprintf - A struct that has two types
+ * struct objprintf - A struct that has two types;
  * a char type and a function type
  * @op: char type
  * @f: a function that accepts va_list arg and returns a pointer to a function.
